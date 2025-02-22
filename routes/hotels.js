c6e0b52fac9b98678276acce09ea8610e24f50e4
Teetath Sukhawattana <lightgirix.rawr@gmail.com>
2025-02-22 08:28:59 +0700

    Implement Authentication

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-//const {protect, authorize} = require('../middleware/auth');
+const {protect, authorize} = require('../middleware/auth');
 
 const app = express();
 
 const {getAllHotel, getOneHotel, createHotel, updateHotel, deleteHotel} = require('../controllers/hotels');
 
-router.route('/').get(getAllHotel).post(createHotel);
-router.route('/:id').get(getOneHotel).put(updateHotel).delete(deleteHotel);
+router.route('/').get(getAllHotel).post(protect, authorize('admin'), createHotel);
+router.route('/:id').get(getOneHotel).put(protect, authorize('admin'), updateHotel).delete(protect, authorize('admin'), deleteHotel);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -12,16 +13,21 @@ connectDB();
 
 //Route files
 const hotels = require('./routes/hotels');
+const auth = require('./routes/auth');
 
 //Body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
+
 //Mount routers
 app.use('/api/v1/hotels', hotels);
+app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT || 5001;
 
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection',(err,promise) => {
