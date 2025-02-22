d298d3ab3c0909e6750a65628fc07424407d0bd7
Teetath Sukhawattana <lightgirix.rawr@gmail.com>
2025-02-22 07:18:16 +0700

    Add more models

---
+++ models/Rooms.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const RoomSchema = new mongoose.Schema({
+    hotel_id : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : Hotels,
+        required: [true]
+    },
+    room_number : {
+        type: String,
+        required: [true, 'Please add a room number']
+    },
+    room_type : {
+        type: String
+    },
+    room_size : {
+        type: Number,
+        required: [true, 'Please add a room size']
+    },
+    price_per_night: {
+        type: Number,
+        required: [true, 'Please add the price per night']
+    },
+    features: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Features
+    }]
+});
