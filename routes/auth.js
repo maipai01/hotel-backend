9bb3d4ef1cbadd10b3cd7f6366798402a4704857
WinterAfterFall <Tonnam.jit@gmail.com>
2025-03-02 01:29:30 +0700

    Add authentication to PromoteUser

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,12 @@ const {register, login, promoteUser , getMe, logout} = require('../controllers/a
 
 const router = express.Router();
 
-const {protect} = require('../middleware/auth');
+const {protect,authorize} = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.get('/logout', logout);
-router.put('/promote/:UserId', promoteUser);
+router.put('/promote/:UserId',protect, authorize(['admin']),promoteUser);
 
 module.exports = router;
