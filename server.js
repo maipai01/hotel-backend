ec03f30968363ec4e57e2677328785890fab78ac
Natthawit La-ongsiri <maipai2006@gmail.com>
2025-03-25 20:11:40 +0700

    change in server

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: 'http://localhost:5001/api/v1'
+                url: process.env.HOST + ":" + PORT + '/api/v1'
             }
         ],
         components: {
@@ -122,7 +122,11 @@ app.use('/api/v1/bookings', bookings);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT, 
+    console.log(
+        `Server running in 
+        ${process.env.NODE_ENV} on ${process.env.HOST} : ${PORT}`));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection',(err,promise) => {
